2a06c18882f27a7c6ea880bb50550433f3a752b2
woohoo-pl <raporty@woohoo.pl>
2025-09-12 10:05:02 +0200

    Hide page heading, update stDecoration background

--- kod.py
+++ kod.py
@@ -14,6 +14,17 @@ st.set_page_config(
     layout="wide",
 )
 
+st.markdown("""
+<style>
+/* Wyłączenie i ukrycie paska dekoracji */
+div[data-testid="stDecoration"] {
+    background-image: none !important;
+    background: none !important;
+    height: 0 !important;
+    display: none !important;
+}
+</style>
+""", unsafe_allow_html=True)
 
 zmienne = ['PLEC','WIEK4','tematyka','wielkosc','Zauważalność']
 reklama = pd.read_csv('12_reklama.txt', usecols = zmienne, sep = '\t', decimal = ",")
@@ -26,18 +37,18 @@ reklama = reklama.replace('kobiece rozrywka/historie/people','magazyny poradnicz
 reklama = reklama.replace('społeczne','magazyny opinii (społeczno-polityczne)')
 reklama = reklama.replace('telewizyjne','magazyny telewizyjne')
 
-st.markdown("<h1 style='margin-top: -70px; text-align: center;'>Zauważalność reklam w prasie</h1>", unsafe_allow_html=True)
+# st.markdown("<h1 style='margin-top: -70px; text-align: center;'>Zauważalność reklam w prasie</h1>", unsafe_allow_html=True)
 
-col1, col2, col3 = st.columns([1.6,2,1])
-with col2:
-    st.markdown("""<div style="font-size:20px; font-weight:bold">Średnia zauważalność reklam</div>""", unsafe_allow_html=True)
-    st.markdown("""<div style="font-size:12px">(prawdopodobieństwo, że reklama będzie zauważona)</div>""", unsafe_allow_html=True)
-    st.markdown("""<div style="font-size:12px"></div>""", unsafe_allow_html=True)
+# col1, col2, col3 = st.columns([1.6,2,1])
+# with col2:
+#     st.markdown("""<div style="font-size:20px; font-weight:bold">Średnia zauważalność reklam</div>""", unsafe_allow_html=True)
+#     st.markdown("""<div style="font-size:12px">(prawdopodobieństwo, że reklama będzie zauważona)</div>""", unsafe_allow_html=True)
+#     st.markdown("""<div style="font-size:12px"></div>""", unsafe_allow_html=True)
 
-col1, col2, col3 = st.columns([0.5,2,1])
-with col2:    
-    obrazek = "zauw.jpg"
-    st.image(obrazek, caption='',  width = 690)
+# col1, col2, col3 = st.columns([0.5,2,1])
+# with col2:    
+#     obrazek = "zauw.jpg"
+#     st.image(obrazek, caption='',  width = 690)
 
 col1, col2, col3 = st.columns([0.9,3,0.1])
 with col2:
